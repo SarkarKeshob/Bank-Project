2a6715e4bb2c0d18395907fb490f3e7cc397ecfd
Shop Queen Dot Com Dot Bd <keshob.sarkar.shuvo@gmail.com>
2023-12-13 23:35:17 +0600

    Uploading After correcting the error messages on login page

--- js/login.js
+++ js/login.js
@@ -8,29 +8,25 @@ loginButton.addEventListener('click',function(){
     const email=document.getElementById('email');
     const password=document.getElementById('password');
     const checkBox=document.getElementById('remember');
-    let inputBox = document.getElementById('input-box');
-    let errorMessage= document.createElement('p');
+    let showError = document.getElementById('show-error');
 
 
-    if((email.value.toLowerCase()===myEmail.toLowerCase()) && (password.value===myPassword) && (checkBox.checked===true)){
+    if((email.value.toLowerCase()===myEmail.toLowerCase()) && (password.value===myPassword)){
         window.location.href="bank.html"
     }
 
-    else if(((email.value.toLowerCase()!==myEmail.toLowerCase()) || (password.value!==myPassword)) && (checkBox.checked===true)){
-        errorMessage.innerText='Wrong Email or Password !!!'
-        inputBox.appendChild(errorMessage);
-        // console.log('wrong password')
+    else if((email.value.toLowerCase()!==myEmail.toLowerCase()) && (password.value===myPassword)){
+        
+        showError.innerText='Email Does not Match!!!'
     }
    
 
-    else if((email.value.toLowerCase()===myEmail.toLowerCase()) && (password.value===myPassword) && (checkBox.checked!==true)){
-        errorMessage.innerText='Please Check The "Remember" me Box'
-        inputBox.appendChild(errorMessage);
+    else if((email.value.toLowerCase()===myEmail.toLowerCase()) && (password.value!==myPassword)){
+        showError.innerText='Wrong Password'
+        
     }
     else{
-        errorMessage.innerText='Please Fill All The Boxes'
-        inputBox.appendChild(errorMessage);
-        // console.log('wrong input')
+        showError.innerText='Email & Password is not Filled'
     }
 
 })
